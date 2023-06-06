16dac0b70a6645f6fa9e530d3e1f0962b2508bea
Michael Skor <xskor@fi.muni.cz>
2023-06-06 23:20:43 +0200

    feat: UserDetail.tsx deleteitems

--- frontend/src/components/Item/Overview/Item/ItemOverviewItem.tsx
+++ frontend/src/components/Item/Overview/Item/ItemOverviewItem.tsx
@@ -1,6 +1,5 @@
 import {FC, useState} from "react";
 import {Item} from "../../../../models";
-import {useNavigate} from "react-router-dom";
 
 interface ItemOverviewItemProps {
     item: Item,
@@ -10,11 +9,12 @@ const ItemOverviewItem: FC<ItemOverviewItemProps> = ({item, toggleItem}) => {
     const [checked, toggleChecked] = useState<boolean>(false);
     return <div
         onClick={() => {
-            toggleChecked(item.blocked && toggleItem(item) && !checked);
+            toggleChecked(!item.blocked && toggleItem(item) && !checked);
         }}
         className={
         "flex flex-row h-48 m-8 rounded-md justify-between transition-all shadow-lg shadow-gray-300 hover:shadow-xl pr-2 pb-2 ".concat(checked ? "bg-red-100 hover:bg-red-200" : (item.blocked ? "bg-yellow-100 hover:bg-yellow-200" : "bg-blue-100 hover:bg-blue-200"))
-    }>
+    }
+    style={{display: item.blocked ? "none" : "inherit"}}>
         <section className=" text-lg flex flex-row h-auto">
             <figure>
                 <img src={item.image}
