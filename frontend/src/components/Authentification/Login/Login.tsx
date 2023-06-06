3073f7775f0453ff008894755039f0f08d67008f
Jaroslav Plšek <xplsek@fi.muni.cz>
2023-06-06 17:37:19 +0200

    feat: login style

--- frontend/src/components/Authentification/Login/Login.tsx
+++ frontend/src/components/Authentification/Login/Login.tsx
@@ -36,25 +36,35 @@ const Login: FC = () => {
             .catch(() => setSuccess(false));
     }
 
-    return <div>
-        <h2>Login</h2>
+    return (
+    <div className="  w-2/4 bg-slate-400 rounded-lg shadow shadow-slate-700 mx-auto p-4 mt-4
+                    flex flex-col space-y-4">
+        <h2 className=" text-xl font-bold leading-tight tracking-tight">
+            Sign in to your account
+        </h2>
         {!success &&
-            <form action="#" onSubmit={handleSubmit(onSubmit)}>
+            <form action="#" onSubmit={handleSubmit(onSubmit)} className="space-y-4">
                 <div>
-                    <label htmlFor="email">Username:</label>
-                    <input type="text" {...register("username")} />
+                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your login:</label>
+                    <input type="text" {...register("username")} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-1" placeholder="name@company.com || login" />
                     {isSubmitted && errors.username && <span>{errors.username.message}</span>}
                 </div>
                 <div>
-                    <label htmlFor="password">Password:</label>
-                    <input type="password" {...register("password")} />
+                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Your password:</label>
+                    <input type="password" {...register("password")} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-1"/>
                     {isSubmitted && errors.password && <span>{errors.password.message}</span>}
                 </div>
-                <button className="green-button" type="submit">Login</button>
+                <button type="submit" className=" w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm py-1.5">
+                    Login
+                </button>
                 {success === false && <i>Bad credentials.</i>}
-            </form>}
-        <NavLink to="/">Back</NavLink>
+            </form>
+        }
+        <NavLink to="/" className=" font-bold text-black hover:underline">
+            Back
+        </NavLink>
     </div>
+    );
 }
 
 export default Login;
