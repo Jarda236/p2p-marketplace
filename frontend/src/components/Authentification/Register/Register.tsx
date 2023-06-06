e6fbc91a70ff2a6269c45d39d03904fd75c4678a
Jaroslav Plšek <xplsek@fi.muni.cz>
2023-06-06 18:25:47 +0200

    feat: style and change register

--- frontend/src/components/Authentification/Register/Register.tsx
+++ frontend/src/components/Authentification/Register/Register.tsx
@@ -43,31 +43,55 @@ const Register: FC = () => {
             .catch(() => changeSuccess(false));
     }
 
-    return <>
-        <h1>Register Page</h1>
+    return (
+        <div className="  w-2/4 bg-slate-400 rounded-lg shadow shadow-slate-700 mx-auto p-4 my-4
+        flex flex-col space-y-4 max-w-sm">
+        <h2 className=" text-xl font-bold leading-tight tracking-tight" >
+            Register account
+        </h2>
         {success ?
             <h3>Úspešne ste sa zaregistrovali!</h3> :
-            <form action="#" onSubmit={handleSubmit(onSubmit)}>
+            <form action="#" onSubmit={handleSubmit(onSubmit)} className="space-y-4">
                 <div>
-                    <label htmlFor="username">Username:</label>
-                    <input type="text" {...register("username")} />
+                    <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900">Username:</label>
+                    <input type="text" {...register("username")} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-1"/>
                     {isSubmitted && errors.username && <span>{errors.username.message}</span>}
                 </div>
                 <div>
-                    <label htmlFor="password">Password:</label>
-                    <input type="password" {...register("password")} />
+                    TODO
+                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Email:</label>
+                    <input type="email" placeholder="john@gmail.com"        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-1"/>
+                </div>
+                <div>
+                    TODO
+                    <label htmlFor="phoneNumber" className="block mb-2 text-sm font-medium text-gray-900">Phone number:</label>
+                    <input type="number" placeholder="123123123"          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-1"/>
+                </div>
+                <div>
+                    TODO
+                    <label htmlFor="city" className="block mb-2 text-sm font-medium text-gray-900">City:</label>
+                    <input type="text"          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-1"/>
+                </div>
+                <div>
+                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password:</label>
+                    <input type="password" {...register("password")} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-1"/>
                     {isSubmitted && errors.password && <span>{errors.password.message}</span>}
                 </div>
                 <div>
-                    <label htmlFor="passwordRepeat">Repeat password:</label>
-                    <input type="password" {...register("passwordRepeat")} />
+                    <label htmlFor="passwordRepeat" className="block mb-2 text-sm font-medium text-gray-900">Repeat password:</label>
+                    <input type="password" {...register("passwordRepeat")} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-1" />
                     {isSubmitted && errors.passwordRepeat && <span>{errors.passwordRepeat.message}</span>}
                 </div>
-                <button type="submit">Register</button>
+                <button type="submit" className=" w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm py-1.5">
+                    Register
+                </button>
                 {success === false && <i>Username is taken.</i>}
             </form>}
-        <NavLink to="/">Back</NavLink>
-    </>
+        <NavLink to="/" className=" font-bold text-black hover:underline">
+            Back
+        </NavLink>
+    </div>
+    );
 }
 
 export default Register;
